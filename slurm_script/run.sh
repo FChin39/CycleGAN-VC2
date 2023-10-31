16c7b98ebe033040ba0c86fa43f174f630e5bd76
Freidrichin <57663487+FChin39@users.noreply.github.com>
2023-10-31 11:31:38 +0800

    Update run.sh

--- slurm_script/run.sh
+++ slurm_script/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=SCSEGPU_M1 
-#SBATCH --qos=q_amsai 
+#SBATCH --qos=q_dmsai 
 #SBATCH --nodes=1 
-#SBATCH --gres=gpu:8
-#SBATCH --mem=8G
+#SBATCH --gres=gpu:1
+#SBATCH --mem=30G
+#SBATCH --cpus-per-tasks=10
+
 
 cd ../
 python train.py
