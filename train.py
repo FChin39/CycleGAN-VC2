1ebed3b16424fa4555d0bed4db687edb552a3aa3
Freidrichin <57663487+FChin39@users.noreply.github.com>
2023-11-06 02:45:33 +0800

    1

--- train.py
+++ train.py
@@ -482,8 +482,8 @@ if __name__ == '__main__':
     coded_sps_A_norm = './cache/coded_sps_A_norm.pickle'
     coded_sps_B_norm = './cache/coded_sps_B_norm.pickle'
     model_checkpoint = './model_checkpoint/'
-    resume_training_at = './model_checkpoint/_CycleGAN_CheckPoint_1e'
-    # resume_training_at = None
+    # resume_training_at = './model_checkpoint/_CycleGAN_CheckPoint_1e'
+    resume_training_at = None
 
     validation_A_dir_default = './data/clean/'
     output_A_dir_default = './converted_sound/clean'
